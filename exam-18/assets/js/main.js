cd7fd3de5cf37cb6f93b11cb9d890e1d0816f7c1
NilSagor <masum1201@gmail.com>
2017-05-21 11:00:44 +0600

    psd tutorial
    100 section from rrf online

---
+++ exam-18/assets/js/main.js
@@ -0,0 +1,18 @@
+(function($){
+	"user strict";
+	jQuery(document).ready(function($){
+	$(".client-testimonial-carousel").owlCarousel({
+		items:3,
+		autoplay:false,
+		loop:true,
+		margin:30,
+		dots:true,
+		nav:false,
+
+	})
+	});
+	jQuery(window).load(function(){
+
+	});
+
+}(jQuery));
