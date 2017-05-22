096157c2e2170941b78a883b122be8c08bc5d1b5
NilSagor <masum1201@gmail.com>
2017-05-22 09:34:29 +0600

    class-17
    100 sections from rrf online horizontal timeline

---
+++ class-27/assets/js/main.js
@@ -0,0 +1,17 @@
+(function($){
+	"user strict";
+	jQuery(document).ready(function($){
+	$(".homepage-slides").owlCarousel({
+		items:1,
+		autoplay:true,
+		loop:true,		
+		dots:true,
+		nav:false,
+
+	})
+	});
+	jQuery(window).load(function(){
+
+	});
+
+}(jQuery));
