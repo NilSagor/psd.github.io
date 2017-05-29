45f39483235b56e5a0d777121b14ecac2b57a52b
NilSagor <masum1201@gmail.com>
2017-05-29 11:42:44 +0600

    class-3 full width video
    full width video from 100 sections of  rrfonline

---
+++ class-3/assets/js/main.js
@@ -0,0 +1,11 @@
+(function($){
+	"use strict";
+	jQuery(document).ready(function($){
+		$(".video-play-btn").magnificPopup({
+			type:'video',
+		});
+	});
+	jQuery(window).load(function(){
+
+	});
+}(jQuery));
