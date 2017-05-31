9c295148d4a0f77405240428b7df2d12c02eb995
NilSagor <masum1201@gmail.com>
2017-05-31 22:18:03 +0600

    class-32 box-promo-area
    box-promo-area from 100 section rrfoline

---
+++ class-32/assets/js/main.js
@@ -0,0 +1,35 @@
+(function($){
+	"user strict";
+	jQuery(document).ready(function($){
+	$(".homepage-slides").owlCarousel({
+		items:1,
+		autoplay:true,
+		loop:true,		
+		dots:true,
+		nav:false,
+
+	});
+	 var s=skrollr.init({
+	 	render:function(data){
+	 		//debug
+	 	}
+	 });
+	 $(window).scroll(function(){
+	 		if ($(this).scrollTop() > 100) {
+	 			$('.scrollToTop').fadeIn();
+	 		} else {
+	 			$('.scrollToTop').fadeOut();
+	 		}
+	 	});
+	 	
+	 	//Click event to scroll to top
+	 	$('.scrollToTop').click(function(){
+	 		$('html, body').animate({scrollTop : 0},800);
+	 		return false;
+	 	});
+	});
+	jQuery(window).load(function(){
+
+	});
+
+}(jQuery));
